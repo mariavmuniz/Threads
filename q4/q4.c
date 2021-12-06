5d4795d3a2bf24bd0adecd3811c7631068e21ac1
Mavi <soaresmunizm@gmail.com>
2021-12-06 15:56:35 -0300

    Updating question 4

--- q4/q4.c
+++ q4/q4.c
@@ -11,39 +11,75 @@ typedef struct{
 
 
 typedef struct{
-  int R, G, B;
-}pixelCor;
+  double R, G, B;
+  int index;
+} PixelCor;
 
-pixelCor *bitmap; //matriz que fica cada pixel
+PixelCor *bitmap; //vetor que fica cada pixel
+int numThreads;  //quantidade de threads
+
+
+PixelCor createPar(double r, double g, double b, int i){ //função que configura o par que será passando por parâmentro
+  PixelCor P;
+  P.R = r;
+  P.G = g;
+  P.B = b;
+  P.index = i;
+  return P;
+}
+
+void* converteTonsdeCinza(void*args){ //função que converte para os tons de cinza
+
+  double r = ((PixelCor*)args)-> R;
+  double g = ((PixelCor*)args)-> G;
+  double b = ((PixelCor*)args)-> B;
+  int i = ((PixelCor*)args)-> index;
+
+  double tomCinza = 0;
+
+  tomCinza = (r*0.30) + (g*0.59) + (b*0.11);
+
+  bitmap[i].R=tomCinza;
+  bitmap[i].G=tomCinza;
+  bitmap[i].B=tomCinza;
+
+  printf("Cinza=%.0lf\n", tomCinza);
+
+}
 
 void leBitmap(Args* cabecalho, FILE* arqBitmap){
    
-  int i, j, limite;
+  int i, j;
+  int rc;
+  PixelCor P;
+  pthread_t *threadsBitmap;
 
   // lendo as duas primeiras linhas do arquivo
     fscanf(arqBitmap, "%s", cabecalho->numeroMagico);
     fscanf(arqBitmap, "%d %d", &cabecalho->qtdLinhas, &cabecalho->qtdColunas);
     fscanf(arqBitmap, "%d", &cabecalho->valorMaximo);
 
-  //alocando a matriz do bitmap
-    limite = cabecalho->qtdLinhas*cabecalho->qtdColunas;
-    bitmap = malloc (limite*sizeof (int*)) ;
+  //alocando a Vetor e as Threads do bitmap
+    numThreads = cabecalho->qtdLinhas*cabecalho->qtdColunas;
+    bitmap = (PixelCor*) malloc (numThreads*sizeof (PixelCor)) ;
+    threadsBitmap = (pthread_t*) malloc(numThreads*sizeof(pthread_t));
 
-    
-    printf("%d\n", limite);
-    
-  //lendo a matriz de bitmap
-    for(i=0;i<limite;i++){
-      fscanf(arqBitmap, "%d %d %d", &bitmap[i].R, &bitmap[i].G, &bitmap[i].B);
+  //lendo a vetor do bitmap e criando as threads
+    for(i=0;i<numThreads;i++){
+      fscanf(arqBitmap, "%lf %lf %lf", &bitmap[i].R, &bitmap[i].G, &bitmap[i].B);
+      P = createPar(bitmap[i].R, bitmap[i].G, bitmap[i].B, i);
+      rc = pthread_create(&threadsBitmap[i], NULL, converteTonsdeCinza,&P);
     }
   //prints de checagem 
+    printf("numThreads=%d\n", numThreads);
     printf("numeroMagico=%s\n", cabecalho->numeroMagico); 
     printf("qtdLinhas=%d\n", cabecalho->qtdLinhas); 
     printf("qtdColunas=%d\n", cabecalho->qtdColunas); 
     printf("valorMaximo=%d\n", cabecalho->valorMaximo); 
 
-    for(i=0;i<limite;i++){
-      printf("linha[%d] =%d %d %d\n", i, bitmap[i].R, bitmap[i].G, bitmap[i].B );
+
+    for(i=0;i<numThreads;i++){
+      printf("linha[%d] =%.0lf %.0lf %.0lf\n", i, bitmap[i].R, bitmap[i].G, bitmap[i].B );
     }
 
     fclose(arqBitmap);
@@ -55,6 +91,7 @@ int main(){
 
   FILE *arqBitmap; //criar a variável para manipular o arquivo
 
+
   arqBitmap = fopen("bitmap1.ppm", "r"); //abertura do arquivo de input
 
   if(arqBitmap == NULL){
@@ -62,14 +99,11 @@ int main(){
   }
   else{
     leBitmap(&cabecalho, arqBitmap);
+    
   }
 
-  
-
-
   free(bitmap);
-
-
+  pthread_exit(NULL);
 
   return 0;
 }
