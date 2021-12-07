340ae97e5318fcad61c5601c51e6ae9eda274e78
Andresa <andresa.almeida@ufpe.br>
2021-12-07 20:07:57 -0300

    Updating q2.c

--- q2.c
+++ q2.c
@@ -5,103 +5,84 @@
 
 #define TAM 10
 
-
 int vetor[TAM];
-double n;
-int ordenado=0;
+int n;
+int ordenado = 0;
 
-//estrutura que contém o índice de ínicio e fim da partição
+//Estrutura que contém o índice de ínicio e fim da partição.
   typedef struct{ 
-    int inicio, fim;
+    int inicio;
+    int tam;
   }Par;
 
-
-//função que configura o par que será passando por parâmentro
-  Par createPar(int i, int f){ 
-    Par P;
-    P.inicio = i;
-    P.fim = f;
-    return P;
-  }
-
-//função que verifica se as partições estão ordenadas entre si
-  void verificaExtremos(int inicio, int fim)
-  { 
-    if(vetor[inicio]> vetor[fim+1] && fim<n){
-      printf("Não esta ordenado\n");
+//Função que verifica se as partições estão ordenadas entre si.
+  void verificaExtremos(int inicio, int fim){ 
+    if(vetor[inicio] > vetor[fim + 1] && fim < TAM){
+      printf("Os extremos não estão ordenados!\n");
       exit(0);
     }
     ordenado++;
   }
 
-//função que verifica se as partições estão ordenadas
-  void* verificaOrdenacao(void*args) 
-  {
+//Função que verifica se as partições estão ordenadas
+  void* verificaOrdenacao(void*args){
     int i;
-    int inicio = ((Par*)args)-> inicio;
-    int fim = ((Par*)args)-> fim;
-    int verifica;
-  
-    for(i=inicio;i<TAM-1;i++){
-      if(vetor[i]>=vetor[i+1]){
-        printf("Não esta ordenado!\n");
-        exit(0);
+
+    int inicio = ((Par*)args) -> inicio;
+    int tam = ((Par*)args) -> tam;
+
+    int fim = inicio + tam;
+
+    printf("Na funcao inicio: %d e fim %d\n", inicio, fim);
+    if(inicio != fim){
+      for(i = inicio; i < fim; i++){
+        if(vetor[i] > vetor[i + 1]){
+          printf("vetor i:%d      ----      vetor i + 1:%d\n", vetor[i], vetor[i + 1]);
+          printf("Não esta ordenado!\n");
+          exit(0);
+        }
       }
     }
 
     verificaExtremos(inicio, fim);
-    if(ordenado == n ){
-     printf("Esta ordenado!\n");
+    if(ordenado == n){
+      printf("Esta ordenado!\n");
     }
-  
   }
 
-int main()
-{
+int main(){
+  int i;
 
-  int i, final = 0, rc, idThread = 0;
-  double particao = 0;
-
-  pthread_t *threads;
-
-  for (i = 0; i < 10; i++)
-  {
+  for (i = 0; i < 10; i++){
     scanf("%d", &vetor[i]);
   }
 
-  scanf("%lf", &n);
+  scanf("%d", &n);
+  pthread_t *threads = (pthread_t *) malloc(n*sizeof(pthread_t));
 
-  threads = (pthread_t *) malloc(n*sizeof(pthread_t));
-  particao = ceil(TAM/ n);
+  int tam1 = TAM / n;
+  int tam2 = TAM - (((TAM / n) * (n - 1)));
 
-  for (i = 0; i < TAM; i = i + particao)
-  {
-    final = i + (particao - 1);
+  Par args;
+  args.inicio = 0;
+  args.tam = tam1 - 1;
+  for(i = 0; i < n - 1; i++){
+    printf("Na main inicio: %d, fim: %d\n", args.inicio, (args.inicio + args.tam));
+    pthread_create(&threads[i], NULL, verificaOrdenacao, (void *) &args);
+    args.inicio += (i < n ? tam1 : tam2);
+  }
 
-    if (final > TAM)
-    {
-      final = TAM - 1;
-    }
-    Par P = createPar(i, final);
-    pthread_create(&threads[idThread], NULL, verificaOrdenacao,&P);
-    idThread++;
-  }  
+  //Atualizando a estrutura de argumentos para a última thread.
+  args.tam = tam2 - 1;
+  printf("Na main inicio: %d, fim: %d\n", args.inicio, (args.inicio + args.tam));
 
-  for(i=0;i<n;i++){
-    pthread_join(threads[i],NULL);
+  //Criando a última theread que irá analisar  N - ((N / n) *(n - 1)) linhas.
+  pthread_create(&threads[i], NULL, verificaOrdenacao, (void*) &args);
+
+  //Unindo as threads.
+  for(i = 0; i < n; i++){
+    pthread_join(threads[i], NULL);
   }
  
   pthread_exit(NULL);
-  
-  return 0;
 }
-
-/*
-Casos testes:
-1=> 1 2 3 4 5 6 7 8 9 10
-    3
-2=> 1 2 2 4 5 6 7 8 8 10 (ta dando errado)
-    3
-
-
-*/
