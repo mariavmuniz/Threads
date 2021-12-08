37b2c4a16b338f4bf1e4fa36fc1842dcf58d162a
Andresa <andresa.almeida@ufpe.br>
2021-12-08 04:49:06 -0300

    Adding comments to question 9

--- q9.c
+++ q9.c
@@ -4,33 +4,45 @@
 #include <stdlib.h>
 #include<math.h>
 
+//Criando a estrutura que irá representar a região crítica.
 typedef struct {
   bool position;
   pthread_mutex_t mutex;
 }Vetor;
 
+//Inicializando as variáveis necessárias.
 Vetor *vetor;
 int t, n;
 pthread_t *threads;
 
 void crivo(int x){
+  //Travando a região crítica da posição atual.
   pthread_mutex_lock(&vetor[x].mutex);
   if(vetor[x].position){
+    //Marcando como falso todos os últimos do valor atual.
     for(int j = 2 * x; j < n; j += x){
+      //Travando a região crítica do múltiplo atual.
       pthread_mutex_lock(&vetor[j].mutex);
       vetor[j].position = false;
+      //Destravando a região crítica do múltiplo atual.
       pthread_mutex_unlock(&vetor[j].mutex);
     }
   }
+  //Destravando a região crítica da posição atual.
   pthread_mutex_unlock(&vetor[x].mutex);
 
+  //Encontrando qual o próximo valor que não está marcado.
   int sqrt_n = sqrt(n) + 1;
   for(int i = x + t; i < sqrt_n; ++i){
+    //Travando a região crítica da posição atual.
     pthread_mutex_lock(&vetor[i].mutex);
     bool aux = vetor[i].position;
     int valor = i;
+    //Destravando a região crítica da posição atual.
     pthread_mutex_unlock(&vetor[i].mutex); 
+    //Analisando se a posição é válida.
     if(aux){
+      //Chamando recursivamente a função para a posição válida atual.
       crivo(valor);
     }
   }
@@ -38,6 +50,7 @@ void crivo(int x){
 
 void *fthread(void* px){
   int x = *((int*)px);
+  //Chamando a função que analisa o vetor.
   crivo(x);
   pthread_exit(NULL);
 }
@@ -46,16 +59,19 @@ int main(){
   scanf(" Numero T: %d", &t);
   scanf(" Numero N: %d", &n);
 
+  //Alocando o vetor.
   vetor = (Vetor*) malloc(n * sizeof(Vetor));
   for(int i = 2; i < n; ++i){
     vetor[i].position = true;
   }
 
+  //Alocando e criando a quantidade t de threads para t elementos do vetor.
   threads = (pthread_t*) malloc(t * sizeof(pthread_t));
   for(int i = 0; i < t; ++i){
     pthread_create(&threads[i], NULL, fthread, (void*) &i);
   }
   
+  //Unindo as threads.
   for(int i = 0; i < t; ++i){
     pthread_join(threads[i], NULL);
   }
@@ -68,7 +84,9 @@ int main(){
   }
   puts("");
 
+  //Desalocando a memória.
   free(vetor);
   free(threads);
+
   pthread_exit(NULL);
 } 
